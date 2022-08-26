8944e6f66d83967b5309fd1e888388f8dc115701
Gaurav-jaybhay <59550071+Gaurav-jaybhay@users.noreply.github.com>
2022-08-26 13:40:32 +0530

    Add files via upload

---
+++ SQL_DAY2_Assignment.sql
@@ -0,0 +1,99 @@
+--Gaurav Jaybhay
+--FIS
+--DAY 2
+
+
+create table tblDEPT(
+deptno int primary key,
+dname varchar(30),
+loc varchar(30)
+)
+
+create table tblEMP(
+empno int primary key,
+ename varchar(30),
+job varchar(30),
+mgr_id int,
+hiredate date,
+sal int,
+comm int,
+deptno int references tblDEPT(deptno))
+
+insert into tblDEPT values(10,'ACCOUNTING','NEW YORK'),
+(20,'RESEARCH','DALLAS'),
+(30,'SALES','CHICAGO'),
+(40,'OPERATIONS','BOSTON')
+
+select * from tblDEPT
+
+insert into tblEMP (empno,ename,job,mgr_id,hiredate,sal,comm,deptno) values
+(7369,'SMITH','CLERK',7902,'1980-12-17',800,null,20),
+(7499,'ALLEN','SALESMAN',7698,'1981-02-20',1600,300,30),
+(7521,'WARD','SALESMAN', 7698,'1981-02-22',1250,500,30),
+(7566,'JONES','MANAGER',7839,'1981-04-02',2975,null,20),
+(7654,'MARTIN','SALESMAN',7698,'1981-09-28',1250,1400,30),
+(7698,'BLAKE','MANAGER',7839,'1981-05-01',2850,NULL,30),
+(7782,'CLARK','MANAGER',7839,'1981-06-09',2450,NULL,10),
+(7788,'SCOTT','ANALYST',7566,'1987-04-19',3000,NULL,20),
+(7839,'KING','PRESIDENT',NULL,'1981-11-17',5000,NULL,10),
+(7844,'TURNER','SALESMAN',7698,'1981-09-08',1500,0,30),
+(7876,'ADAMS','CLERK',7788,'1987-05-23',1100,NULL,20),
+(7900,'JAMES','CLERK',7698,'1981-12-03',950,NULL,30),
+(7902,'FORD','ANALYST',7566,'1981-12-03',3000,NULL,20),
+(7934,'MILLER','CLERK',7782,'1982-01-23',1300,NULL,10)
+
+select * from tblEMP
+
+--1. List all employees whose name begins with 'A'. 
+select * from tblEMP where ename like 'A%'
+
+--2. Select all those employees who don't have a manager. 
+select * from tblEMP where mgr_id IS NULL
+
+--3. List employee name, number and salary for those employees who earn in the range 1200 to 1400. 
+select ename,empno,sal from tblEMP where sal between 1200 and 1400
+
+--4. Give all the employees in the RESEARCH department a 10% pay rise. 
+--Verify that this has been done by listing all their details before and after the rise. 
+select * from tblEMP
+update tblEMP set sal=sal+(sal*0.10) where deptno=(select deptno from tblDEPT where dname='RESEARCH')
+select * from tblEMP
+
+--5. Find the number of CLERKS employed. Give it a descriptive heading. 
+select count(job) as 'Number of CLERKS Employed' from tblEMP where job='CLERK'
+
+--6. Find the average salary for each job type and the number of people employed in each job. 
+select job as 'Job Type',avg(sal) as 'Average Salary',count(empno) as'No of Employees' from tblEMP 
+group by job
+
+--7. List the employees with the lowest and highest salary. 
+select min(sal) as 'Minimum Salary',max(sal) as 'Maximum Salary' from tblEMP
+
+--8. List full details of departments that don't have any employees. 
+select * from tblDEPT where deptno not in (select deptno from tblEMP)
+
+--9. Get the names and salaries of all the analysts earning more than 1200 who are based in department 20. Sort the answer by ascending order of name. 
+select ename as 'Employee Name',sal as 'Salary' from tblEMP 
+where sal>1200 and job='ANALYST' and deptno=20
+order by ename
+
+--10. For each department, list its name and number together with the total salary paid to employees in that department. 
+select deptno,count(empno) as 'No of Employees', sum(sal) as 'Total Salary' from tblEMP
+group by deptno
+
+--11. Find out salary of both MILLER and SMITH.
+select ename,sal as 'Salary' from tblEMP where ename ='MILLER' or ename='SMITH'
+
+--12. Find out the names of the employees whose name begin with ‘A’ or ‘M’. 
+select ename as 'Employee Name' from tblEMP where ename like '[AM]%'
+
+--13. Compute yearly salary of SMITH. 
+select sal*12 as 'Yearly salary',ename from tblEMP where ename='SMITH'
+
+--14. List the name and salary for all employees whose salary is not in the range of 1500 and 2850. 
+select ename,sal from tblEMP where sal not between 1500 and 2850
+
+--15. display list of managers who have more than 2 employees reporting to them
+select count(empno) as 'No of Employees',mgr_id from tblEMP 
+group by mgr_id
+having(count(empno)>2)
